6e33e02287496d27a0a4307ea94bc998f751941d
jsieon97 <rudtn0820@naver.com>
2024-05-23 23:51:13 +0900

    dijkstra_Algorithm

---
+++ dijkstra/dijkstra.py
@@ -0,0 +1,47 @@
+# 6-2-1
+from queue import PriorityQueue
+
+INF = int(1e18)
+
+
+def dijkstra(N, E, X, Z):
+    selected = [False] * (N + 1)
+    dist = [INF] * (N + 1)
+    pq = PriorityQueue()
+
+    dist[X] = 0
+    pq.put([0, X])
+
+    while not pq.empty():
+        cost, here = pq.get()
+
+        if dist[here] < cost or selected[here] == True:
+            continue
+
+        selected[here] = True
+
+        if here == Z:
+            return dist[here]
+
+        for there, c in E[here]:
+            next_dist = cost + c
+
+            if selected[there] == False and dist[there] > next_dist:
+                dist[there] = next_dist
+                pq.put([next_dist, there])
+
+    return -1
+
+
+def solution(N, M, edges, X, Z):
+    E = list([] for _ in range(N + 1))
+    for p, c, d in edges:
+        E[p].append([c, d])
+
+    return dijkstra(N, E, X, Z)
+
+
+N, M = map(int, input().split())
+edges = list(list(map(int, input().split())) for _ in range(M))
+X, Z = map(int, input().split())
+print(solution(N, M, edges, X, Z))
